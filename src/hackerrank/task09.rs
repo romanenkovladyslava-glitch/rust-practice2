bd0437133d4bd76ffed7484baa7fde426fc77ddc
romanenkovladyslava-glitch <romanenko.vladyslava@e-u.edu.ua>
2026-05-20 18:05:39 +0300

    task09.rs

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,54 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        int pairs = 0;
+        int[] count = new int[101];
+
+        for (int i = 0; i < n; i++)
+        {
+            count[ar[i]]++;
+        }
+
+        for (int i = 0; i < count.Length; i++)
+        {
+            pairs += count[i] / 2;
+        }
+
+        return pairs;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
