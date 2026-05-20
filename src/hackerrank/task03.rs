e66d49dbb629552595b3defb172417180749b977
romanenkovladyslava-glitch <romanenko.vladyslava@e-u.edu.ua>
2026-05-20 17:42:41 +0300

    task03.сs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,36 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+    
+
+    public static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
