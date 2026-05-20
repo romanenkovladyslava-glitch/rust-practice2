7aee82368a675ee2cfe0955ed13a44d64bce7073
romanenkovladyslava-glitch <romanenko.vladyslava@e-u.edu.ua>
2026-05-20 17:47:50 +0300

    task08.cs

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,65 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER n
+     * 2. INTEGER_ARRAY ar
+     */
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        HashSet<int> colors = new HashSet<int>();
+        int pairs = 0;
+
+        foreach (int sock in ar)
+        {
+            if (colors.Contains(sock))
+            {
+                pairs++;
+                colors.Remove(sock);
+            }
+            else
+            {
+                colors.Add(sock);
+            }
+        }
+
+        return pairs;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
